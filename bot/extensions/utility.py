272866cd9546441ebd84fb53f2d71c36f5481be8
VegitoTy <126sservices@gmail.com>
2022-06-30 10:59:04 +0530

    Yeet

--- bot/extensions/utility.py
+++ bot/extensions/utility.py
@@ -27,6 +27,36 @@ async def _av(ctx: lightbulb.Context) -> None:
     embed.set_image(member.display_avatar_url)
     await ctx.respond(embed=embed, reply=True)
 
+@plugin.command()
+@lightbulb.option("text", "the content of the embed", required=True, modifier=lightbulb.commands.base.OptionModifier(3))
+@lightbulb.option("channel", "the channel to send the embed to", type=hikari.GuildChannel, default=None, required=False)
+@lightbulb.option("color", "color of the embed (without #)", required=False, default=None)
+@lightbulb.command("embed", "Creates an embed with the specified color in the specified channel, separate the title from the description with |")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def _embed(ctx : lightbulb.Context) -> None:
+    channel = ctx.options.channel
+    text = ctx.options.text
+    color = ctx.options.color
+    
+    if not channel:
+        cid = ctx.event.message.channel_id
+    else:
+        cid = channel.id
+    
+    if not color:
+        colorcode = bot_config['color']['default']
+    else:
+        colorcode = color
+    
+    try:
+        title, description = text.split("|", 1)
+        embed = hikari.Embed(title=title, description=description, color=colorcode)
+        await ctx.app.rest.create_message(cid, embed=embed)
+        await ctx.event.message.add_reaction("✅")
+    except:
+        await ctx.event.message.add_reaction("❌")
+    
+
 def load(bot):
     bot.add_plugin(plugin)
 
